100e0537435ff7b7166a89b922928e8beeb8be8c
Joseph Turner <turner@miserware.com>
2014-03-07 11:41:29 -0500

    Couple more problems, clean up some boilerplate

--- Euler.hs
+++ Euler.hs
@@ -3,6 +3,8 @@ module Euler
         eulerProblem
     ) where
 
+import Data.List (nub, group)
+
 divides :: Integer -> Integer -> Bool
 divides a b = (b `mod` a) == 0
 
@@ -12,15 +14,11 @@ pFactors x = pFactorsHelper [] x
           pFactorsHelper xs n = pFactorsHelper (divisor:xs) (n `div` divisor)
             where divisor = head (dropWhile (not . (`divides` n)) [2..n])
 
-rle :: (Eq a) => [a] -> [[a]]
-rle all@(x:xs) = run:(rle rest)
-    where sRun = span (x==) all
-          run = fst sRun
-          rest = snd sRun
-rle _ = []
+uniqueFactors :: Integer -> [Integer]
+uniqueFactors = nub . pFactors
 
 rleCount :: (Eq a) => [a] -> [(a, Int)]
-rleCount = (map counts) . rle
+rleCount = (map counts) . group
     where counts all@(x:xs) = (x, length all)
 
 factorCounts = rleCount . pFactors
@@ -35,11 +33,15 @@ isPalindrome s = s' == (reverse s')
     where s' = show s
 
 eulerProblem :: Int -> String
-eulerProblem 1 = show ( sum (filter ismult [1..999]))
+eulerProblem 1 = (show . sum . filter ismult) [1..999]
                  where ismult n = (divides 3 n) || (divides 5 n)
-eulerProblem 2 = show (sum (filter even (takeWhile (<4000000) fibs)))
-eulerProblem 3 = show (maximum (pFactors 600851475143))
-eulerProblem 4 = show (maximum (filter isPalindrome
-                    (concat
-                        (map (\x -> map (*x) [100..x]) [100..999]))))
+eulerProblem 2 = (show . sum . filter even . takeWhile (<4000000)) fibs
+eulerProblem 3 = (show . maximum . pFactors) 600851475143
+eulerProblem 4 = (show . maximum . filter isPalindrome . concat
+                        . map (\x -> map (*x) [100..x])) [100..999]
+eulerProblem 5 = (show . product . map largestMult . uniqueFactors . product) [1..n]
+                 where n = 20
+                       largestMult x = x ^ (floor (logBase (fromInteger x) (fromInteger n)))
+eulerProblem 6 = show (((^2) . sum) xs - (sum . map (^2)) xs)
+                 where xs = [1..100]
 eulerProblem _ = "Not done yet!"
